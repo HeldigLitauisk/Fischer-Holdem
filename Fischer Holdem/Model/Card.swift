c6e2fe2d31e6c8bec73e7e294f5963dad52762e0
omglithuania <eimantasurbutis@gmail.com>
2018-02-17 23:21:04 +0100

    First Commit

---
+++ Fischer Holdem/Model/Card.swift
@@ -0,0 +1,49 @@
+//
+//  Card.swift
+//  Fischer Holdem
+//
+//  Created by Eimantas Urbutis on 17/02/2018.
+//  Copyright © 2018 Urbut Corp. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+class Card: SCNNode {
+    let cardValue: (rank: Rank, suit: Suit)
+    
+    init(cardValue: (Rank, Suit), nodeName: String = "card") {
+        self.cardValue = cardValue
+        super.init()
+        self.name = nodeName
+        self.geometry = SCNBox(width: 15, height: 21, length: 0.5, chamferRadius: 5)
+        self.geometry?.firstMaterial?.shininess = 1
+        self.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        self.physicsBody?.categoryBitMask = CollisionCategoryCard
+        self.physicsBody?.collisionBitMask = CollisionCategoryCard | CollisionCategoryChip | CollisionCategoryDealerButton
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    enum Suit: Int {
+        case heart = 0, daimond, club, spade
+    }
+    
+    enum Rank: Int {
+        case deuce = 0, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
+    }
+    
+    func deckBuilder() -> Array<(rank: Rank, suit: Suit)> {
+        var deck: Array<(rank: Rank, suit: Suit)> = []
+        for _ in 1...52 {
+            let randomRank = Rank(rawValue: Int(arc4random_uniform(13)))
+            let randomSuit = Suit(rawValue: Int(arc4random_uniform(4)))
+            deck.append((rank: randomRank!, suit: randomSuit!))
+        }
+        return deck
+    }
+    
+    
+}
---
+++ SharedConstants.swift
@@ -0,0 +1,11 @@
+//
+//  SharedConstants.swift
+//  Fischer Holdem
+//
+//  Created by Eimantas Urbutis on 17/02/2018.
+//  Copyright © 2018 Urbut Corp. All rights reserved.
+//
+let CollisionCategoryDealerButton = 4
+let CollisionCategoryChip = 6
+let CollisionCategoryCard = 8
+let CollisionCategoryFloor = 10
